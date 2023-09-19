828cb8fdf8914ebd60f3b533d43ff021d7b74e2a
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2023-09-19 09:53:48 +0900

    Add winston logger

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import { app } from './app';
+import Logger from './util/logger';
+
+const logger = Logger.getInstance();
 
 app.listen(app.get('port'), () => {
-	console.log(`Server is running on port ${app.get('port')}`);
+	logger.info(`Server is running on port ${app.get('port')}`);
 });
