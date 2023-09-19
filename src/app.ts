8da29d0934189af789b11c7df4f4e4605516f941
gkqkehs7 <77993709+gkqkehs7@users.noreply.github.com>
2023-09-19 14:25:56 +0900

    Change port

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.all('/*', (req: Request, res: Response, next: NextFunction) => {
 	res.header('Access-Control-Allow-Headers', 'X-Requested-With');
 	next();
 });
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 8000);
 app.use(helmet({ contentSecurityPolicy: false }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
